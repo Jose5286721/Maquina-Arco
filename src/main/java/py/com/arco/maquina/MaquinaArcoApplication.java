ec94c0c1c24fa5d3b3f7fa1d0856aff9f931e348
jose-arco <jose-arco@MacBook-Pro-2.local>
2020-11-04 11:58:43 -0300

    1.0

---
+++ src/main/java/py/com/arco/maquina/MaquinaArcoApplication.java
@@ -0,0 +1,13 @@
+package py.com.arco.maquina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaquinaArcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaquinaArcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/py/com/arco/maquina/MaquinaArcoApplicationTests.java
@@ -0,0 +1,13 @@
+package py.com.arco.maquina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaquinaArcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
